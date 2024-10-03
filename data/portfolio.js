f4babb05f16847d11767b5a848461165ae50ed6e
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-10-03 15:50:55 +0700

    initial commit

---
+++ data/portfolio.js
@@ -0,0 +1,85 @@
+const header = {
+  wordmark: '',
+}
+
+const about = {
+  names: ['SPACEMAN', '1O1'],
+  greetingEmoji: '👩‍🚀',
+  role: 'Junior Software Developer',
+  company: 'Generation Thailand',
+  description: [
+    'I graduated from Generation Thailand`s Junior Software Developer Bootcamp, specializing in designing, developing, and deploying front-end and back-end web applications. With a strong commitment to creating high-quality software, I am a dedicated team player who is eager to learn, self-motivated, and capable of working independently.',
+  ],
+  resume:
+    'https://drive.google.com/file/d/11Oar6PRvh8b-S-yNe5HUP3HHzqRUqP2E/view?usp=sharing',
+  social: {
+    github: 'https://github.com/Voltran1c',
+    linkedin: 'https://www.linkedin.com/in/watcharapong1996/',
+  },
+}
+
+const projects = [
+  {
+    thumbnail: '/images/kick it up-image.png',
+    name: 'Kick It Up',
+    description: [
+      'A shoe-selling website featuring login, register, home, and product search functionalities for a smooth shopping experience.',
+    ],
+    stack: ['React', 'JavaScript', 'TailwindCSS'],
+    sourceCode: 'https://github.com/jsd7-group7-fremen',
+    livePreview: 'https://kick-it-up.vercel.app/',
+  },
+  {
+    thumbnail: '/images/typing test-image.png',
+    name: 'Typing Test',
+    description: [
+      'A simple website to practice typing skills with a timer for real-time speed assessment.',
+    ],
+    stack: ['React', 'Redux', 'JavaScript'],
+    sourceCode: 'https://github.com/Voltran1c/typing-speed-test.git',
+    livePreview: 'https://typing-test-app-dusky.vercel.app/',
+  },
+  {
+    thumbnail: '/images/nexusbot-image.png',
+    name: 'Nexus Bot',
+    description: [
+      'A chatbot website powered by GPT-4 API, offering intelligent conversation features',
+    ],
+    stack: ['Next.js', 'JavaScript', 'TailwindCSS'],
+    sourceCode: 'https://github.com/Voltran1c/Chatbot-AI.git',
+    livePreview: 'https://chatbot-jade-phi.vercel.app/',
+  },
+  {
+    thumbnail: '/images/airbnb clone-image.png',
+    name: 'Airbnb Clone',
+    description: [
+      'A visually appealing clone of the Airbnb website, styled using Tailwind CSS for a modern design.',
+    ],
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/Voltran1c/Airbnb-Clone.git',
+    livePreview: 'https://airbnb-clone-tau-nine.vercel.app/',
+  },
+]
+
+const skills = [
+  'React',
+  'JavaScript',
+  'TypeScript',
+  'Tailwind CSS',
+  'Material UI',
+  'Ant Design',
+  'Redux',
+  'Git',
+  'Node.js',
+  'Express.js',
+  'MongoDB',
+  'Next.js',
+  'SQL',
+  'Postman',
+]
+
+const contact = {
+  email: 'Watcharapong.k.korn@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
